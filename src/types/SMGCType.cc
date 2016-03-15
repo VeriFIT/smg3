17d70da1da89a79d34b30df3a5a400ff7bab7506
Petr Peringer <peringer@fit.vutbr.cz>
2016-03-15 18:19:08 +0100

    prototype implementation of types

--- src/types/SMGCType.cc
+++ src/types/SMGCType.cc
@@ -1,15 +1,53 @@
+
+// Basic type hierarchy for SMG implementation
+// (c) PP, 2016
+
 #include "SMGCType.hh"
+
+#include <memory>
+#include <sstream>
 
 namespace smg {
 
-const SMGCType SMGCType::CreateTypeWithSize(const int size) { return SMGCType(size); }
 
-const SMGCType SMGCType::GetIntType() { return CreateTypeWithSize(SMGCType::SIZE_INT); }
+std::ostream &operator << (std::ostream &s, TypeId t) {
+    return s << t.toString();
+}
 
-const SMGCType SMGCType::GetPointerType() { return CreateTypeWithSize(SMGCType::SIZE_POINTER); }
+void Type::add_field(unsigned o, std::string name, TypeId t) {
+    // TODO: assert( isStruct() || isUnion() )
+    field_desc fld = { o, name, t };
+    fields.push_back(fld);
+}
 
-SMGCType::SMGCType(const int size) : size_(size) {}
+// convert Type to string representation
+std::string Type::toString() const {
+    std::ostringstream s;
+    s << "Type#" << fid << " ";
+    switch(kind) {
+    case INT:
+        s << "Integer" << bsize << "(sizeof=" << size() << ")";
+        break;
+    case PTR:
+        s << "Pointer(sizeof=" << size() << ") to Type#" << targetType.id();
+        break;
+    case STRUCT:
+        s << "Struct(sizeof=" << size() << ", {\n";
+        for(field_desc f: fields)
+            s << " offset=" << f.offset << " name=" << f.name
+              << " type=#" << f.type.id() << "\n";
+        s << "})";
+        break;
+    case ARRAY:
+        s << "Array["<<bsize<<"](sizeof=" << size() << ") of " << targetType.toString();
+        break;
+    default:
+        s << "UNKNOWN";
+        break;
+    }
+    return s.str();
+}
 
-int SMGCType::GetSize() const { return size_; }
+Type::map_t Type::m;  // definition of static member
 
 }  // namespace smg
--- src/types/SMGCType.hh
+++ src/types/SMGCType.hh
@@ -1,20 +1,124 @@
 #pragma once
 
+// Basic type hierarchy for SMG implementation
+// (c) PP, 2016
+
+#include <string>
+#include <map>
+#include <vector>
+#include <memory>
+#include <iostream>
+
 namespace smg {
 
-class SMGCType {
- private:
-  static const int SIZE_INT = 4;
-  static const int SIZE_POINTER = 8;
-  const int size_;
+typedef int frontend_id;    // unique GCC id (or LLVM representation)
 
- public:
-  static const SMGCType CreateTypeWithSize(const int size);
-  static const SMGCType GetIntType();
-  static const SMGCType GetPointerType();
+class Type;
 
-  explicit SMGCType(const int size);
-  int GetSize() const;
+// TypeId -- reference to type representation (copyable, assignable)
+class TypeId {
+    Type *p;    // target owned by registry, should be valid
+    TypeId(Type *ptr) : p{ptr} {}
+    friend class Type;
+  public:
+    TypeId(): p{nullptr} {};
+    TypeId(const TypeId &) = default;
+    TypeId&operator=(const TypeId &) = default;
+
+    unsigned size() const;
+    unsigned GetSize() const { return size(); }
+    std::string toString() const;
+    frontend_id id() const;
+
+    bool isInteger() const;
+    bool isPointer() const;
+    bool isStruct() const;
+    bool isUnion() const;
+    bool isArray() const;
+
+    // TODO: remove
+    void add_field(unsigned o, std::string name, TypeId t);
 };
+
+// type output
+std::ostream &operator << (std::ostream &s, TypeId t);
+
+// Type representation (not all members are valid for a type)
+class Type final {
+  frontend_id fid;      // front-end type representation
+  unsigned sz;          // sizeof
+  unsigned bsize;       // int width, float width, array dimension
+  TypeId targetType;    // ptr, array, return type
+
+  struct field_desc {   // for struct/union members, ...
+    unsigned offset;    // always zero for union
+    std::string name;   // .name
+    TypeId type;
+  };
+  std::vector<field_desc> fields; // struct/union fields
+public:
+  enum Kind { UNKNOWN, VOID, INT, REAL, PTR, STRUCT, UNION, ARRAY, FUNCTION, };
+private:
+  Kind kind = UNKNOWN;
+public:
+  // generic wrapper
+  template<typename T1, typename ... T>
+  static TypeId create(T1 x, T ... args) {
+     if(mapped(x))
+        return m[x].get();
+     // type not registered yet -- create new and register
+     std::unique_ptr<Type> t{new Type(x,args...)};
+     m[x] = std::move(t); // TODO use emplace?
+     return m[x].get();
+  }
+
+private: // constructors
+  Type(frontend_id id, unsigned size, unsigned bs): fid(id), sz{size}, bsize{bs}, kind{INT} {}
+  Type(frontend_id id, unsigned size, TypeId t): fid(id), sz{size}, targetType(t), kind{PTR} {}
+  Type(frontend_id id, unsigned size): fid(id), sz{size}, kind{STRUCT} {}
+  Type(frontend_id id, TypeId t, unsigned n): fid(id), sz{t.size()*n}, bsize(n), targetType(t), kind{ARRAY} {}
+  Type() = delete;
+  Type(const Type&) = delete;           // not copyable
+  Type&operator=(const Type&) = delete; // not assignable
+
+public:
+  unsigned      size() const { return sz; } // sizeof
+  frontend_id   id() const { return fid; }
+
+  bool          isInteger() const { return kind==INT; }
+  bool          isPointer() const { return kind==PTR; }
+  bool          isStruct() const { return kind==STRUCT; }
+  bool          isUnion() const { return kind==UNION; }
+  bool          isArray() const { return kind==ARRAY; }
+
+  std::string   toString() const;
+  void          add_field(unsigned o, std::string name, TypeId t);
+  static TypeId get(frontend_id id) {
+     if(mapped(id))     return m[id].get();
+     else               return nullptr; // TODO: throw bad_frontend_type_id
+  }
+private:
+  // map registry for all types, updated dynamically
+  // TODO: use singleton?
+  typedef std::map<frontend_id,std::unique_ptr<Type>> map_t;
+  static map_t m; // owns all types
+  static bool mapped(frontend_id id) { return (m.find(id)!=m.end()); }
+}; // class Type
+
+// class Type methods definitions
+inline unsigned TypeId::size() const { return p->size(); }
+inline frontend_id TypeId::id() const { return p->id(); }
+
+inline bool TypeId::isInteger() const { return p->isInteger(); }
+inline bool TypeId::isPointer() const { return p->isPointer(); }
+inline bool TypeId::isStruct() const { return p->isStruct(); }
+inline bool TypeId::isUnion() const { return p->isUnion(); }
+inline bool TypeId::isArray() const { return p->isArray(); }
+
+inline std::string TypeId::toString() const { return p->toString(); }
+inline void TypeId::add_field(unsigned o, std::string name, TypeId t) { p->add_field(o,name,t); }
+
+// TODO: remove/rename
+typedef class TypeId SMGCType;
 
 }  // namespace smg
